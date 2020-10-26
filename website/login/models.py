18b601d0c7382fa0e7edc92f90eba1ae68e90ae5
Lokesh <cs18b026@iittp.ac.in>
2020-10-26 19:53:07 +0530

    connected to database

--- website/login/models.py
+++ website/login/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Donation(models.Model):
+    donorname=models.CharField(max_length=100)
+    fooditem=models.CharField(max_length=100)
+    quantity=models.CharField(max_length=100)
+    foodtiming=models.CharField(max_length=100)
+    date=models.CharField(max_length=100)
+    time=models.CharField(max_length=100)
+    address=models.TextField()
